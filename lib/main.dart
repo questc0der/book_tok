e484769244b67d2b977e89b56ab009b8bff37a6c
pour-moi <pour64moi@gmail.com>
2025-05-08 23:41:30 +0300

    moved to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screen/home.dart';
 
 void main() {
   runApp(App());
@@ -11,40 +12,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        initialIndex: 0,
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [Tab(text: "For You"), Tab(text: "Following")],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              PageView(
-                controller: controller,
-                scrollDirection: Axis.vertical,
-                children: <Widget>[
-                  Container(color: Colors.green),
-                  Container(color: Colors.yellow),
-                  Container(color: Colors.red),
-                ],
-              ),
-              PageView(
-                controller: controller,
-                children: <Widget>[
-                  Container(color: Colors.green),
-                  Container(color: Colors.yellow),
-                  Container(color: Colors.redAccent),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
 }
