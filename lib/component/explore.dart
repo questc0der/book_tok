5e47970459c8b51dd9b1c5558fd90ec62a5aa335
pour-moi <pour64moi@gmail.com>
2025-05-23 02:44:12 +0300

    built book card UI

--- lib/component/explore.dart
+++ lib/component/explore.dart
@@ -12,37 +12,108 @@ class _ExploreState extends State<ExploreView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Explore")),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: SearchAnchor(
-          builder: (BuildContext context, SearchController controller) {
-            return SearchBar(
-              controller: controller,
+      body: Column(children: [_buildSearchBar(), _buildViewCard()]),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: SearchAnchor(
+        builder: (BuildContext context, SearchController controller) {
+          return SearchBar(
+            padding: WidgetStatePropertyAll(
+              EdgeInsetsDirectional.symmetric(horizontal: 20),
+            ),
+            controller: controller,
+            onTap: () {
+              controller.openView();
+            },
+            onChanged: (_) {
+              controller.openView();
+            },
+            leading: Icon(Icons.search),
+          );
+        },
+        suggestionsBuilder: (
+          BuildContext context,
+          SearchController controller,
+        ) {
+          return List<ListTile>.generate(5, (int index) {
+            final String item = 'item $index';
+            return ListTile(
+              title: Text(item),
               onTap: () {
-                controller.openView();
+                setState(() {
+                  controller.closeView(item);
+                });
               },
-              onChanged: (_) {
-                controller.openView();
-              },
-              leading: Icon(Icons.search),
             );
-          },
-          suggestionsBuilder: (
-            BuildContext context,
-            SearchController controller,
-          ) {
-            return List<ListTile>.generate(5, (int index) {
-              final String item = 'item $index';
-              return ListTile(
-                title: Text(item),
-                onTap: () {
-                  setState(() {
-                    controller.closeView(item);
-                  });
-                },
-              );
-            });
-          },
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildViewCard() {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 28.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Image.asset(
+              "assets/pictures/reading.png",
+              width: 130,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(width: 20),
+            Column(
+              children: [
+                Text(
+                  "Book Title",
+                  style: TextStyle(
+                    fontFamily: 'Circular',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text("Author", style: TextStyle(fontFamily: 'Circular')),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Icon(Icons.favorite_border_rounded),
+                    Text("1.2K"),
+                    SizedBox(width: 7),
+                    Icon(Icons.remove_red_eye_outlined),
+                    Text("1.2K"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(40, 20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        ),
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text(
+                        "Read",
+                        style: TextStyle(fontFamily: 'Circular'),
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Icon(Icons.bookmark_border_outlined),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
