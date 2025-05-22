bf47b290e6b73b0a36528247c321223c62df7108
pour-moi <pour64moi@gmail.com>
2025-05-22 05:10:14 +0300

    providers

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,10 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import "./models/user.dart";
 
-final name = StateProvider<String>((ref) {
-  return "Biruk in River";
-});
-
 final userInfo = ChangeNotifierProvider<User>((ref) {
   return User(name: "", email: "", age: "", gender: "", image: null);
 });
