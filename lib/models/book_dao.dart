cb4763cab9c1de7c877a3808ffa66493a21c67ff
pour-moi <pour64moi@gmail.com>
2025-07-08 14:43:51 +0300

    add BookDao class for Firestore book management and remove unused Preview widget

---
+++ lib/models/book_dao.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class BookDao extends ChangeNotifier {
+  final db = FirebaseFirestore.instance;
+
+  Future<String> post(
+    String title,
+    String content,
+    String author,
+    String imageLink,
+    String uid,
+  ) async {
+    try {
+      DocumentReference docRef = await db.collection('books').add({
+        'title': title,
+        'content': content,
+        'author': author,
+        'image': imageLink,
+        'uid': uid,
+      });
+      return docRef.id;
+    } catch (e) {
+      print(e);
+    }
+    return "Error";
+  }
+}
