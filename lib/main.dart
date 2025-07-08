24c55babe61dce5649a2ea279deba2df7ee3c850
pour-moi <pour64moi@gmail.com>
2025-07-08 14:46:29 +0300

    refactor: implement routing with GoRouter and update main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,53 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screen/Login/welcome.dart';
+import 'package:go_router/go_router.dart';
+import 'screen/home.dart';
+import "./screen/Login/signup.dart";
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ProviderScope(child: App()));
 }
+
+final GoRouter _router = GoRouter(
+  redirect: (BuildContext context, GoRouterState state) {
+    return FirebaseAuth.instance.currentUser != null ? '/home' : '/login';
+  },
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Welcome();
+      },
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (BuildContext context, GoRouterState state) {
+        return Home();
+      },
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (BuildContext context, GoRouterState state) {
+        return SignUp();
+      },
+    ),
+  ],
+);
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Welcome());
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+    );
   }
 }
