5fcda4f73046d746f5d7953adbea6ce2bc357157
pour-moi <pour64moi@gmail.com>
2025-07-08 14:44:58 +0300

    refactor: clean up providers by removing commented code and organizing imports

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,13 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'models/user_dao.dart';
+import 'models/book_dao.dart';
 
 final userDaoProvider = ChangeNotifierProvider<UserDao>((ref) {
   return UserDao();
 });
 
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import "./models/user.dart";
-
-// final userInfo = ChangeNotifierProvider<User>((ref) {
-//   return User(name: "", email: "", age: "", gender: "", image: null);
-// });
+final postDaoProvider = ChangeNotifierProvider<BookDao>((ref) {
+  return BookDao();
+});
