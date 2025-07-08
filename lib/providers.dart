bb649755e9b4522913ee2847488aaa812c137c5e
pour-moi <pour64moi@gmail.com>
2025-07-08 09:48:24 +0300

    refactor providers to use UserDao for state management

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,6 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import "./models/user.dart";
+import 'models/user_dao.dart';
 
-final userInfo = ChangeNotifierProvider<User>((ref) {
-  return User(name: "", email: "", age: "", gender: "", image: null);
+final userDaoProvider = ChangeNotifierProvider<UserDao>((ref) {
+  return UserDao();
 });
+
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import "./models/user.dart";
+
+// final userInfo = ChangeNotifierProvider<User>((ref) {
+//   return User(name: "", email: "", age: "", gender: "", image: null);
+// });
