a308befb89dca4f9d1ae3c39c156a07088d096e0
pour-moi <pour64moi@gmail.com>
2025-07-11 09:16:58 +0300

    refactor: update ExploreView to use ConsumerStatefulWidget and enhance book data handling in view card

--- lib/component/explore.dart
+++ lib/component/explore.dart
@@ -1,15 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import '../models/book.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ExploreView extends StatefulWidget {
+class ExploreView extends ConsumerStatefulWidget {
   const ExploreView({super.key});
 
   @override
-  State<ExploreView> createState() => _ExploreState();
+  ConsumerState<ExploreView> createState() => _ExploreState();
 }
 
-class _ExploreState extends State<ExploreView> {
+class _ExploreState extends ConsumerState<ExploreView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,81 +58,104 @@ class _ExploreState extends State<ExploreView> {
     );
   }
 
+  Future<List<Map<String, dynamic>>> fetchBooks() async {
+    final db = FirebaseFirestore.instance;
+    final querySnapshot = await db.collection('books').get();
+    return querySnapshot.docs.map((doc) => doc.data()).toList();
+  }
+
   Widget _buildViewCard() {
-    return Expanded(
-      child: ListView.builder(
-        padding: EdgeInsets.symmetric(horizontal: 28.0),
-        itemCount: books.length,
-        itemBuilder: (context, index) {
-          return Card(
-            margin: EdgeInsets.only(bottom: 16.0), // spacing between cards
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Image.asset(
-                    books[index].image,
-                    width: 130,
-                    height: 190,
-                    fit: BoxFit.fill,
-                  ),
-                  SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: fetchBooks(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+        final books = snapshot.data!;
+        return Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.symmetric(horizontal: 28.0),
+            itemCount: books.length,
+            itemBuilder: (context, index) {
+              final bookData = books[index];
+
+              return Card(
+                margin: EdgeInsets.only(bottom: 16.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
                     children: [
-                      SizedBox(
-                        width: 190,
-                        child: Text(
-                          books[index].title,
-                          style: TextStyle(
-                            fontFamily: 'Circular',
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                      Image.network(
+                        bookData['image'],
+                        width: 130,
+                        height: 190,
+                        fit: BoxFit.fill,
+                      ),
+                      SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 190,
+                            child: Text(
+                              bookData['title'],
+                              style: TextStyle(
+                                fontFamily: 'Circular',
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      Text("Author", style: TextStyle(fontFamily: 'Circular')),
-                      SizedBox(height: 20),
-                      Row(
-                        children: [
-                          Icon(Icons.favorite_border_rounded),
-                          Text("1.2K"),
-                          SizedBox(width: 7),
-                          Icon(Icons.remove_red_eye_outlined),
-                          Text("1.2K"),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              minimumSize: Size(40, 20),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(8.0),
+                          Text(
+                            bookData['author'],
+                            style: TextStyle(fontFamily: 'Circular'),
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Icon(Icons.favorite_border_rounded),
+                              Text("1.2K"),
+                              SizedBox(width: 7),
+                              Icon(Icons.remove_red_eye_outlined),
+                              Text("1.2K"),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              TextButton(
+                                onPressed: () {},
+                                style: TextButton.styleFrom(
+                                  minimumSize: Size(40, 20),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(8.0),
+                                    ),
+                                  ),
+                                  backgroundColor: Colors.black,
+                                  foregroundColor: Colors.white,
+                                ),
+                                child: Text(
+                                  "Read",
+                                  style: TextStyle(fontFamily: 'Circular'),
                                 ),
                               ),
-                              backgroundColor: Colors.black,
-                              foregroundColor: Colors.white,
-                            ),
-                            child: Text(
-                              "Read",
-                              style: TextStyle(fontFamily: 'Circular'),
-                            ),
+                              SizedBox(width: 15),
+                              Icon(Icons.bookmark_border_outlined),
+                            ],
                           ),
-                          SizedBox(width: 15),
-                          Icon(Icons.bookmark_border_outlined),
                         ],
                       ),
                     ],
                   ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
