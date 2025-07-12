28d6ab5b658812a0ed4786845ffd94cfe8eba19e
pour-moi <pour64moi@gmail.com>
2025-07-12 14:56:03 +0300

    Refactor code structure and improve functionality: - Removed unused imports in page_view.dart and book.dart. - Updated image path from .png to .jpg in book.dart. - Added new book entry with detailed summary in book.dart. - Replaced Navigator with GoRouter for navigation in welcome.dart. - Updated ExplorePage to be commented out in explore.dart. - Changed ExploreView reference in home.dart to match new structure. - Adjusted border radius in main.dart for button styling. - Redesigned Profile page layout and added functionality to fetch and display user posts in profile.dart.

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,10 +1,166 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+class Profile extends StatefulWidget {
+  const Profile({super.key});
 
   @override
+  State<Profile> createState() => _ProfilePage();
+}
+
+class _ProfilePage extends State<Profile> with SingleTickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("This is profile page"));
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 28.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 100, width: 100, child: CircleAvatar()),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "User Name",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateColor.transparent,
+                  shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+                child: Text(
+                  "Edit Profile",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // spacing: 70.0,
+            children: [
+              Column(
+                children: [
+                  Text("Followers"),
+                  Text("100", style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+              Column(
+                children: [
+                  Text("Following"),
+                  Text("100", style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+              Column(
+                children: [
+                  Text("Posts"),
+                  Text("100", style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ],
+          ),
+          Expanded(
+            child: DefaultTabController(
+              length: 2,
+              child: Scaffold(
+                appBar: AppBar(bottom: _buildTabBar()),
+                body: TabBarView(
+                  children: <Widget>[_postsView(), _likesView()],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  PageView _postsView() {
+    return PageView(
+      controller: _buildPageController(),
+      children: <Widget>[_buildCardGrid()],
+    );
+  }
+
+  PageView _likesView() {
+    return PageView(
+      controller: _buildPageController(),
+      children: <Widget>[Container(color: Colors.red)],
+    );
+  }
+
+  PageController _buildPageController() {
+    final controller = PageController(initialPage: 0);
+    return controller;
+  }
+
+  TabBar _buildTabBar() {
+    return TabBar(tabs: [Tab(text: 'Posts'), Tab(text: 'Likes')]);
+  }
+
+  Future<List<Map<String, dynamic>>> fetchPostedBooks() async {
+    final db = FirebaseFirestore.instance;
+    final user = FirebaseAuth.instance.currentUser;
+    final querySnapshot =
+        await db
+            .collection('books')
+            .where("email", isEqualTo: user!.email)
+            .get();
+    return querySnapshot.docs.map((doc) => doc.data()).toList();
+  }
+
+  Widget _buildCardGrid() {
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: fetchPostedBooks(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text("Error ${snapshot.error}"));
+        }
+
+        final books = snapshot.data!;
+        return GridView.builder(
+          itemCount: books.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 4,
+            mainAxisSpacing: 4,
+            childAspectRatio: 1,
+          ),
+          itemBuilder: (context, index) {
+            final bookData = books[index];
+            return Column(
+              children: [
+                Expanded(
+                  child: Card(
+                    clipBehavior: Clip.hardEdge,
+                    child: Image.network(bookData['image'], fit: BoxFit.fill),
+                  ),
+                ),
+                Text(bookData['title']),
+              ],
+            );
+          },
+        );
+      },
+    );
   }
 }
