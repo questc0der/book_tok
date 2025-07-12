a2c97afb1ff5b6bca84558dd13b9a0a663c4c1fe
pour-moi <pour64moi@gmail.com>
2025-07-12 14:54:17 +0300

    refactor: update MainView layout to enhance UI and improve user interaction features

--- lib/screen/main.dart
+++ lib/screen/main.dart
@@ -9,65 +9,137 @@ class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final data = book.data() as Map<String, dynamic>;
-    final user = FirebaseAuth.instance.currentUser;
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          Image.network(
-            data['image'],
-            width: double.infinity,
-            fit: BoxFit.cover,
-            height: double.infinity,
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withAlpha(190),
-                    offset: Offset(0, 9),
-                    spreadRadius: 28,
-                    blurRadius: 30,
-                  ),
-                ],
+          Stack(
+            children: [
+              Image.network(
+                data['image'],
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+                height: MediaQuery.of(context).size.height / 3,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    data['title'],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Circular',
-                      fontSize: 30,
-                    ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withAlpha(100),
+                        offset: Offset(0, -3),
+                        spreadRadius: 10,
+                        blurRadius: 15,
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 5),
-                  Text(
-                    data['author'],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Circular',
-                      fontSize: 18,
-                    ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        data['title'],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Circular',
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        data['author'],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Circular',
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    user?.email ?? "Anon",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Circular',
-                      fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    SizedBox(height: 55, width: 55, child: CircleAvatar()),
+                    SizedBox(width: 4),
+                    Column(
+                      children: [
+                        Text(
+                          "User Name",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            shape: WidgetStatePropertyAll(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+                            minimumSize: WidgetStatePropertyAll(Size(70, 30)),
+                          ),
+                          child: Text(
+                            "Follow",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
+
+                Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite),
+                      style: ButtonStyle(
+                        iconColor: WidgetStateProperty.resolveWith<Color>((
+                          states,
+                        ) {
+                          if (states.contains(WidgetState.pressed)) {
+                            return Colors.red;
+                          }
+                          return Colors.grey;
+                        }),
+                      ),
+                    ),
+                    Text("0"),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.mode_comment),
+                      style: ButtonStyle(
+                        iconColor: WidgetStatePropertyAll(Colors.grey),
+                      ),
+                    ),
+                    Text("0"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                data['content'],
+                style: TextStyle(fontFamily: 'Circular'),
               ),
             ),
           ),
