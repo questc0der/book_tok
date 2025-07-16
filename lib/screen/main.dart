7b2583d88d5c579a02e83e11ab68c7b7bb9d97fa
pour-moi <pour64moi@gmail.com>
2025-07-16 10:13:40 +0300

    refactor: restructure MainView for improved content pagination and layout

--- lib/screen/main.dart
+++ lib/screen/main.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MainView extends StatelessWidget {
@@ -9,141 +8,172 @@ class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final data = book.data() as Map<String, dynamic>;
-    return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Image.network(
-                data['image'],
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
-                height: MediaQuery.of(context).size.height / 3,
-              ),
-              Positioned(
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withAlpha(100),
-                        offset: Offset(0, -3),
-                        spreadRadius: 10,
-                        blurRadius: 15,
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        data['title'],
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Circular',
-                          fontSize: 30,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        data['author'],
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Circular',
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    // Split content into pages
+    List<String> paginateContent(String content, int charsPerPage) {
+      List<String> pages = [];
+      for (int i = 0; i < content.length; i += charsPerPage) {
+        pages.add(
+          content.substring(
+            i,
+            (i + charsPerPage > content.length)
+                ? content.length
+                : i + charsPerPage,
+          ),
+        );
+      }
+      return pages;
+    }
+
+    final contentPages = paginateContent(data['content'], 800);
+
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 1 + contentPages.length, // 1 for post + N pages
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Column(
               children: [
-                Wrap(
-                  crossAxisAlignment: WrapCrossAlignment.center,
+                Stack(
                   children: [
-                    SizedBox(height: 55, width: 55, child: CircleAvatar()),
-                    SizedBox(width: 4),
-                    Column(
-                      children: [
-                        Text(
-                          "User Name",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                    Image.network(
+                      data['image'],
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover,
+                      height: MediaQuery.of(context).size.height / 2.2,
+                    ),
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
                         ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            shape: WidgetStatePropertyAll(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8.0),
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withAlpha(100),
+                              offset: Offset(0, -3),
+                              spreadRadius: 10,
+                              blurRadius: 15,
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data['title'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Circular',
+                                fontSize: 30,
+                                color: Colors.white,
                               ),
                             ),
-                            minimumSize: WidgetStatePropertyAll(Size(70, 30)),
-                          ),
-                          child: Text(
-                            "Follow",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              data['author'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Circular',
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ],
                 ),
-
-                Wrap(
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.favorite),
-                      style: ButtonStyle(
-                        iconColor: WidgetStateProperty.resolveWith<Color>((
-                          states,
-                        ) {
-                          if (states.contains(WidgetState.pressed)) {
-                            return Colors.red;
-                          }
-                          return Colors.grey;
-                        }),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          CircleAvatar(radius: 27.5),
+                          SizedBox(width: 4),
+                          Column(
+                            children: [
+                              Text(
+                                "User Name",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(70, 30),
+                                  backgroundColor: Colors.white,
+                                  foregroundColor: Colors.black,
+                                ),
+                                child: Text(
+                                  "Follow",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                    ),
-                    Text("0"),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.mode_comment),
-                      style: ButtonStyle(
-                        iconColor: WidgetStatePropertyAll(Colors.grey),
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.favorite),
+                            color: Colors.grey,
+                          ),
+                          Text("0"),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.mode_comment),
+                            color: Colors.grey,
+                          ),
+                          Text("0"),
+                        ],
                       ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Text(
+                      contentPages[0],
+                      style: TextStyle(fontFamily: 'Circular', fontSize: 16),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 9,
                     ),
-                    Text("0"),
-                  ],
+                  ),
                 ),
               ],
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Text(
-                data['content'],
-                style: TextStyle(fontFamily: 'Circular'),
+            );
+          } else {
+            // Page 2...N: reading pages only
+            return Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              child: SingleChildScrollView(
+                child: Text(
+                  contentPages[index - 1],
+                  style: TextStyle(
+                    fontSize: 15,
+                    height: 1.5,
+                    fontFamily: 'Circular',
+                  ),
+                ),
               ),
-            ),
-          ),
-        ],
+            );
+          }
+        },
       ),
     );
   }
