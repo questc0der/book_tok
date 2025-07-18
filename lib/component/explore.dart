1099439076a1fcc954e22d910ab87780bca4fee4
pour-moi <pour64moi@gmail.com>
2025-07-18 10:53:18 +0300

    refactor: enhance ExploreView layout with a new slide view and improved book card design

--- lib/component/explore.dart
+++ lib/component/explore.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -15,7 +17,13 @@ class _ExploreState extends ConsumerState<ExploreView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Explore")),
-      body: Column(children: [_buildSearchBar(), _buildViewCard()]),
+      body: Column(
+        children: [
+          _buildSearchBar(),
+          _buildSlideView(),
+          SizedBox(height: 350, child: _buildViewCard()),
+        ],
+      ),
     );
   }
 
@@ -75,87 +83,114 @@ class _ExploreState extends ConsumerState<ExploreView> {
           return Center(child: Text('Error: ${snapshot.error}'));
         }
         final books = snapshot.data!;
-        return Expanded(
-          child: ListView.builder(
-            padding: EdgeInsets.symmetric(horizontal: 28.0),
-            itemCount: books.length,
-            itemBuilder: (context, index) {
-              final bookData = books[index];
+        return ListView.separated(
+          padding: EdgeInsets.symmetric(horizontal: 28.0),
+          scrollDirection: Axis.horizontal,
+          separatorBuilder: (_, __) => SizedBox(width: 12),
+          itemCount: books.length,
+          itemBuilder: (context, index) {
+            final bookData = books[index];
 
-              return Card(
-                margin: EdgeInsets.only(bottom: 16.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    children: [
-                      Image.network(
-                        bookData['image'],
-                        width: 130,
-                        height: 190,
-                        fit: BoxFit.fill,
+            return SizedBox(
+              height: 330,
+              width: 290,
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.network(
+                      bookData['image'],
+                      fit: BoxFit.cover,
+                      height: 330,
+                      width: 290,
+                      errorBuilder:
+                          (context, error, stackTrace) =>
+                              Icon(Icons.broken_image),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 17,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
                       ),
-                      SizedBox(width: 20),
-                      Column(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withAlpha(100),
+                            offset: Offset(0, -1),
+                            spreadRadius: 2,
+                            blurRadius: 7,
+                          ),
+                        ],
+                      ),
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          SizedBox(
-                            width: 190,
-                            child: Text(
-                              bookData['title'],
-                              style: TextStyle(
-                                fontFamily: 'Circular',
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
+                          Text(
+                            bookData['title'],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Circular',
+                              fontSize: 30,
+                              color: Colors.white,
                             ),
                           ),
                           Text(
                             bookData['author'],
-                            style: TextStyle(fontFamily: 'Circular'),
-                          ),
-                          SizedBox(height: 20),
-                          Row(
-                            children: [
-                              Icon(Icons.favorite_border_rounded),
-                              Text("1.2K"),
-                              SizedBox(width: 7),
-                              Icon(Icons.remove_red_eye_outlined),
-                              Text("1.2K"),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              TextButton(
-                                onPressed: () {},
-                                style: TextButton.styleFrom(
-                                  minimumSize: Size(40, 20),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.all(
-                                      Radius.circular(8.0),
-                                    ),
-                                  ),
-                                  backgroundColor: Colors.black,
-                                  foregroundColor: Colors.white,
-                                ),
-                                child: Text(
-                                  "Read",
-                                  style: TextStyle(fontFamily: 'Circular'),
-                                ),
-                              ),
-                              SizedBox(width: 15),
-                              Icon(Icons.bookmark_border_outlined),
-                            ],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Circular',
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              );
-            },
-          ),
+                ],
+              ),
+            );
+          },
         );
       },
+    );
+  }
+
+  Widget _buildSlideView() {
+    final categories = [
+      "Trending",
+      "History",
+      "Adventure",
+      "Comic",
+      "Romance",
+      "Sci-Fi",
+      "Mystery",
+      "Horror",
+    ];
+
+    return SizedBox(
+      height: 60,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        itemCount: categories.length,
+        separatorBuilder: (_, __) => SizedBox(width: 12),
+        itemBuilder: (context, index) {
+          return Chip(
+            label: Text(
+              categories[index],
+              style: TextStyle(fontFamily: 'Circular'),
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            backgroundColor: Colors.grey.shade200,
+          );
+        },
+      ),
     );
   }
 }
